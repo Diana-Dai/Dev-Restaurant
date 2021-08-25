231e7ef85904939ccd39c07602099ffb6de42e79
Diana <69062403+Diana-Dai@users.noreply.github.com>
2021-08-25 18:23:42 +1000

    Immigration to Webpack

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,18 @@
+/* eslint-disable global-require */
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: [require("tailwindcss"), require("autoprefixer")],
-};
+  plugins: [
+    require('postcss-preset-env')({
+      browsers: 'last 2 versions',
+    }),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    purgecss({
+      content: ['./src/index.html'],
+      defaultExtractor: (content) =>
+        content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
+      safelist: ['navbar-open'],
+    }),
+  ],
+}
--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,132 +1,132 @@
-const carouselWrapper = document.querySelector(".carousel-wrapper");
-const carouselContainer = document.querySelector(".carousel-container");
-const slidesNum = carouselWrapper.childElementCount;
-//Start from the second image
-let currentSlide = 1;
-let preSlide = 0;
-
-class CarouselControler {
-  constructor() {
-    this.instance = "";
-    this.controlButtons = true;
-  }
-  start() {
-    // check if it's the first time
-    if (!this.instance) {
-      this.instance = true;
-      utils.addClones();
-      utils.activateCarousel();
-      autoSlide.start();
-    } else {
-      autoSlide.start();
-    }
-  }
-  stop() {
-    autoSlide.stop();
-  }
-}
-
-//Control automatic carousel
-class AutoSlide {
-  constructor() {
-    this.controler = "";
-  }
-  start() {
-    this.controler = setInterval(utils.next, 3000);
-  }
-  stop() {
-    clearInterval(this.controler);
-  }
-}
-const autoSlide = new AutoSlide();
+/* eslint-disable no-debugger */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-use-before-define */
+const carouselWrapper = document.querySelector('.carousel-wrapper')
+const slidesNum = carouselWrapper.childElementCount
+// Mark the status of the carousel
+let isCarouselOn = false
+// Start from the second image
+let currentSlide = 1
+let preSlide = 0
 
 const utils = {
-  next: function () {
-    carouselWrapper.style.transition = "0.8s";
-    preSlide = currentSlide;
-    currentSlide++;
-    utils.startSlider(preSlide, currentSlide);
+  next() {
+    carouselWrapper.style.transition = '0.8s'
+    preSlide = currentSlide
+    currentSlide += 1
+    utils.startSlider(preSlide, currentSlide)
 
     if (currentSlide === slidesNum + 1) {
-      //If currentSlide is the lastfake, change the point to the first one
+      // If currentSlide is the lastfake, change the point to the first one
       setTimeout(() => {
-        utils.resetToStart();
-        currentSlide = 1;
-      }, 800);
+        utils.resetToStart()
+        currentSlide = 1
+      }, 800)
     }
   },
-  startSlider: function () {
-    changeSlides(currentSlide);
-    changePagination(preSlide, currentSlide);
+  startSlider() {
+    changeSlides(currentSlide)
+    changePagination(preSlide, currentSlide)
 
-    function changeSlides(currentSlide) {
-      carouselWrapper.style.transform =
-        "translateX(" + -100 * currentSlide + "%)";
+    function changeSlides(cslide) {
+      carouselWrapper.style.transform = 'translateX(' + -100 * cslide + '%)'
     }
-    function changePagination(preSlide, currentSlide) {
-      const paginations = document.querySelectorAll(".pagination span");
+    function changePagination(prslide, cslide) {
+      const paginations = document.querySelectorAll('.pagination span')
       if (paginations.length > 0) {
-        if (preSlide !== "undefined")
-          if (preSlide === 0) {
-            preSlide = slidesNum;
+        if (prslide !== 'undefined') {
+          if (prslide === 0) {
+            prslide = slidesNum
           }
-        if (preSlide === slidesNum + 1) {
-          preSlide = 1;
+          if (prslide === slidesNum + 1) {
+            prslide = 1
+          }
         }
-        paginations[preSlide - 1].classList.remove("selected");
-        if (currentSlide !== "undefined") {
-          if (currentSlide === 0) {
-            currentSlide = slidesNum;
+        paginations[prslide - 1].classList.remove('selected')
+        if (cslide !== 'undefined') {
+          if (cslide === 0) {
+            cslide = slidesNum
           }
-          if (currentSlide === slidesNum + 1) {
-            currentSlide = 1;
+          if (cslide === slidesNum + 1) {
+            cslide = 1
           }
-          paginations[currentSlide - 1].classList.add("selected");
+          paginations[cslide - 1].classList.add('selected')
         }
       }
     }
   },
-  resetToStart: function () {
-    carouselWrapper.style.transition = "0s";
-    carouselWrapper.style.transform = "translateX( -100%)";
+  resetToStart() {
+    carouselWrapper.style.transition = '0s'
+    carouselWrapper.style.transform = 'translateX( -100%)'
   },
-  prev: function () {
-    carouselWrapper.style.transition = "0.8s";
-    preSlide = currentSlide;
-    currentSlide--;
-    utils.startSlider(preSlide, currentSlide);
-    //If currentSlide is the firstfake, change the point to the last one
+  prev() {
+    carouselWrapper.style.transition = '0.8s'
+    preSlide = currentSlide
+    currentSlide -= 1
+    utils.startSlider(preSlide, currentSlide)
+    // If currentSlide is the firstfake, change the point to the last one
     if (currentSlide === 0) {
       setTimeout(() => {
-        utils.resetToEnd();
-        currentSlide = slidesNum;
-      }, 800);
+        utils.resetToEnd()
+        currentSlide = slidesNum
+      }, 800)
     }
   },
-  resetToEnd: function () {
-    carouselWrapper.style.transition = "0s";
-    carouselWrapper.style.transform = "translateX(" + -slidesNum * 100 + "%)";
+  resetToEnd() {
+    carouselWrapper.style.transition = '0s'
+    carouselWrapper.style.transform = 'translateX(' + -slidesNum * 100 + '%)'
   },
-  //Activate the left and right button to control carousel
-  activateCarousel: function () {
-    const btnLeft = document.querySelector(".control-btn-left");
-    const btnRight = document.querySelector(".control-btn-right");
-    if (btnLeft) btnLeft.addEventListener("click", utils.prev);
-    if (btnRight) btnRight.addEventListener("click", utils.next);
+  // Activate the left and right button to control carousel
+  activateCarousel() {
+    const btnLeft = document.querySelector('.control-btn-left')
+    const btnRight = document.querySelector('.control-btn-right')
+    if (btnLeft) btnLeft.addEventListener('click', utils.prev)
+    if (btnRight) btnRight.addEventListener('click', utils.next)
   },
   //  Add a clone img at the front and at the end
-  addClones: function () {
-    const fakefirst = carouselWrapper.children[0].cloneNode(true);
-    const fakelast = carouselWrapper.children[slidesNum - 1].cloneNode(true);
-    carouselWrapper.insertAdjacentHTML("afterBegin", fakelast.outerHTML);
-    carouselWrapper.insertAdjacentHTML("beforeEnd", fakefirst.outerHTML);
+  addClones() {
+    const fakefirst = carouselWrapper.children[0].cloneNode(true)
+    const fakelast = carouselWrapper.children[slidesNum - 1].cloneNode(true)
+    carouselWrapper.insertAdjacentHTML('afterBegin', fakelast.outerHTML)
+    carouselWrapper.insertAdjacentHTML('beforeEnd', fakefirst.outerHTML)
   },
-};
+}
 
-carouselContainer.onmouseenter = function () {
-  autoSlide.stop();
-};
+const carouselControler = {
+  instance: '',
+  isCarouselOn: '',
+  start() {
+    if (!isCarouselOn) {
+      isCarouselOn = true
 
-carouselContainer.onmouseleave = function () {
-  autoSlide.start();
-};
+      // check if it's the first time
+      if (!this.instance) {
+        this.instance = true
+        utils.addClones()
+        utils.activateCarousel()
+        autoSlide.start()
+      } else {
+        autoSlide.start()
+      }
+    }
+  },
+  stop() {
+    if (isCarouselOn) {
+      isCarouselOn = false
+      autoSlide.stop()
+    }
+  },
+}
+
+// Control automatic carousel
+const autoSlide = {
+  controler: '',
+  start() {
+    this.controler = setInterval(utils.next, 3000)
+  },
+  stop() {
+    clearInterval(this.controler)
+  },
+}
+
+export { carouselControler, autoSlide }
