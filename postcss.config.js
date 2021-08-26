3f4c99593e79268e8c38eaa002baf0231722bcf3
Diana <69062403+Diana-Dai@users.noreply.github.com>
2021-08-26 14:39:54 +1000

    Optimize codes

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,12 @@
 /* eslint-disable global-require */
-const purgecss = require('@fullhuman/postcss-purgecss')
+const purgecss = require("@fullhuman/postcss-purgecss");
 
 module.exports = {
   plugins: [
-    require('postcss-preset-env')({
-      browsers: 'last 2 versions',
+    require("postcss-preset-env")({
+      browsers: "last 2 versions",
     }),
-    require('tailwindcss'),
-    require('autoprefixer'),
-    purgecss({
-      content: ['./src/index.html'],
-      defaultExtractor: (content) =>
-        content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [],
-      safelist: ['navbar-open'],
-    }),
+    require("tailwindcss"),
+    require("autoprefixer"),
   ],
-}
+};
--- src/js/index.js
+++ src/js/index.js
@@ -1,127 +1,180 @@
 /* eslint-disable no-console */
-import '../css/style.css'
+import "../css/style.css";
 
-import { carouselControler, autoSlide } from './carousel'
+import { carouselControler, autoSlide } from "./carousel";
 
 function checkViewPoint(el, scrollTop) {
   const viewPortHeight =
     window.innerHeight ||
     document.documentElement.clientHeight ||
-    document.body.clientHeight
-  const { offsetTop } = el
-  const height = el.offsetHeight
-  const top = offsetTop - scrollTop - viewPortHeight
+    document.body.clientHeight;
+  const { offsetTop } = el;
+  const height = el.offsetHeight;
+  const top = offsetTop - scrollTop - viewPortHeight;
   // When the element comes into the view
-  return top <= 0 && top >= -height - viewPortHeight
+  return top <= 0 && top >= -height - viewPortHeight;
 }
 
-function triggerAnimation(isInViewPoint, animatedElement) {
+function initCarouselAnimation(isInViewPoint, animatedElement) {
   if (isInViewPoint) {
-    animatedElement.start()
+    animatedElement.start();
   } else {
-    animatedElement.stop()
+    animatedElement.stop();
   }
 }
 
 function toggleClass(isActive, animatedElement, className) {
   if (isActive) {
-    animatedElement.classList.add(className)
+    animatedElement.classList.add(className);
   } else {
-    animatedElement.classList.remove(className)
+    animatedElement.classList.remove(className);
   }
 }
 
-function stickyHeader(header, scrollTop) {
-  const height = header.offsetHeight
+function initStickyHeader(header, scrollTop) {
+  const height = header.offsetHeight;
   if (scrollTop > height) {
-    toggleClass(true, header, 'active')
+    toggleClass(true, header, "active");
   } else {
-    toggleClass(false, header, 'active')
+    toggleClass(false, header, "active");
   }
 }
-function changeNodeStatus(item) {
-  // Exclude empty node and text node
-  if (item.nodeName !== '#text' && !/\s/.test(item.nodeValue)) {
-    if (item.classList.contains('active')) {
-      item.classList.remove('active')
+
+function clearAllActiveStatus(el) {
+  el.childNodes.forEach((item) => {
+    // Exclude empty node and text node
+    if (item.nodeName !== "#text" && !/\s/.test(item.nodeValue)) {
+      if (item.classList.contains("active")) {
+        item.classList.remove("active");
+      }
     }
+  });
+}
+
+const menuTabsControler = {
+  tabs: {
+    "dinner-tab": "dinner",
+    "lunch-tab": "lunch",
+    "brunch-tab": "brunch",
+    "desert-tab": "desert",
+    "wine-tab": "wine",
+  },
+  init: function () {
+    document.getElementById("menu-tabs").addEventListener("click", (e) => {
+      this.func(e);
+    });
+  },
+  func: function (e) {
+    let currentTab = "";
+
+    // Check the current tab
+    if (e.target.id || e.target.parentNode.id) {
+      if (this.tabs[e.target.id]) {
+        currentTab = e.target.id;
+      } else if (this.tabs[e.target.parentNode.id]) {
+        currentTab = e.target.parentNode.id;
+      }
+    }
+
+    // Change the status of the menu this.tabs
+    clearAllActiveStatus(document.getElementById("menu-tabs"));
+    document.getElementById(currentTab).classList.add("active");
+
+    [currentTab] = currentTab.split("-");
+    currentTab = "#myMenuTabContent #" + currentTab;
+
+    // Change the status of the menu pane
+    clearAllActiveStatus(document.querySelector("#myMenuTabContent"));
+    document.querySelector(currentTab).classList.add("active");
+  },
+};
+
+const navBarControler = {
+  init: function () {
+    document.getElementById("navbar-toggler").addEventListener("click", () => {
+      return document.body.classList.contains("navbar-open")
+        ? document.body.classList.remove("navbar-open")
+        : document.body.classList.add("navbar-open");
+    });
+    document.getElementById("navbar-close").addEventListener("click", () => {
+      document.body.classList.remove("navbar-open");
+    });
+    document.querySelector("#navbar ul").addEventListener("click", () => {
+      document.body.classList.remove("navbar-open");
+    });
+  },
+};
+
+const carouselHoverControler = {
+  init: function () {
+    // eslint-disable-next-line func-names
+    document.querySelector(".carousel-container").onmouseenter = function () {
+      autoSlide.stop();
+    };
+
+    // eslint-disable-next-line func-names
+    document.querySelector(".carousel-container").onmouseleave = function () {
+      autoSlide.start();
+    };
+  },
+};
+
+const navTabControler = {
+  init: function () {
+    const intersectionObserver = new IntersectionObserver(function (entries) {
+      entries.forEach((item) => {
+        const targetTab = document.getElementById("tab-" + item.target.id);
+        // When the target item is in viewPoint
+        if (item.intersectionRatio > 0 && targetTab) {
+          clearAllActiveStatus(document.getElementById("nav-list"));
+          targetTab.parentNode.classList.add("active");
+        }
+      });
+    });
+    document.querySelectorAll("#nav-list li a").forEach((item) => {
+      const href = item.getAttribute("href");
+      // Start observing
+      intersectionObserver.observe(document.querySelector(href));
+    });
+  },
+};
+const scrollAnimationControler = {
+  init: function () {
+    window.addEventListener("scroll", () => {
+      const { scrollTop } = document.documentElement;
+
+      // Carousel
+      initCarouselAnimation(
+        checkViewPoint(document.querySelector(".customers"), scrollTop),
+        carouselControler
+      );
+
+      // Header
+      initStickyHeader(document.querySelector("header"), scrollTop);
+    });
+  },
+};
+
+class Commander {
+  constructor() {
+    this.stack = [];
+  }
+  init() {
+    this.stack.forEach((item) => {
+      item.init();
+    });
+  }
+  add(action) {
+    this.stack.push(action);
   }
 }
 
 window.onload = () => {
-  // Scroll Control
-  window.addEventListener('scroll', () => {
-    const { scrollTop } = document.documentElement
-
-    // Carousel
-    triggerAnimation(
-      checkViewPoint(document.querySelector('.customers'), scrollTop),
-      carouselControler
-    )
-
-    // Header
-    stickyHeader(document.querySelector('header'), scrollTop)
-  })
-
-  // Navbar Control
-  document.getElementById('navbar-toggler').addEventListener('click', () => {
-    if (!document.body.classList.contains('navbar-open')) {
-      document.body.classList.add('navbar-open')
-      return
-    }
-    document.body.classList.remove('navbar-open')
-  })
-  document.getElementById('navbar-close').addEventListener('click', () => {
-    document.body.classList.remove('navbar-open')
-  })
-  document
-    .getElementById('navbar')
-    .querySelector('ul')
-    .addEventListener('click', () => {
-      document.body.classList.remove('navbar-open')
-    })
-
-  // Menu Tabs Control
-  document.getElementById('menu-tabs').addEventListener('click', (e) => {
-    const tabs = {
-      'dinner-tab': 'dinner',
-      'lunch-tab': 'lunch',
-      'brunch-tab': 'brunch',
-      'desert-tab': 'desert',
-      'wine-tab': 'wine',
-    }
-    let currentTab = ''
-
-    // Check the current tab
-    if (e.target.id || e.target.parentNode.id) {
-      if (tabs[e.target.id]) {
-        currentTab = e.target.id
-      } else if (tabs[e.target.parentNode.id]) {
-        currentTab = e.target.parentNode.id
-      }
-    }
-    // Change the status of the menu tabs
-    document.getElementById('menu-tabs').childNodes.forEach((item) => {
-      changeNodeStatus(item)
-    })
-    document.getElementById(currentTab).classList.add('active')
-
-    // Change the status of the menu pane
-    ;[currentTab] = currentTab.split('-')
-    currentTab = '#myMenuTabContent #' + currentTab
-    document.querySelector('#myMenuTabContent').childNodes.forEach((item) => {
-      changeNodeStatus(item)
-    })
-    document.querySelector(currentTab).classList.add('active')
-  })
-
-  // eslint-disable-next-line func-names
-  document.querySelector('.carousel-container').onmouseenter = function () {
-    autoSlide.stop()
-  }
-
-  // eslint-disable-next-line func-names
-  document.querySelector('.carousel-container').onmouseleave = function () {
-    autoSlide.start()
-  }
-}
+  const commander = new Commander();
+  commander.add(scrollAnimationControler);
+  commander.add(navBarControler);
+  commander.add(menuTabsControler);
+  commander.add(carouselHoverControler);
+  commander.add(navTabControler);
+  commander.init();
+};
